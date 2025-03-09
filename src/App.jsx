86c97b01f760aa70ffa01b26c029175a1329ccb4
Tobi Azeez <azeezoe@appstate.edu>
2025-03-09 13:28:37 -0400

    Testing web app build

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 					mb={8}
 				>
 					<Text as={"span"} bgGradient={"linear(to-r, cyan.400, blue.500)"} bgClip={"text"}>
-						My Besties
+						My Besties!!
 					</Text>
 					🚀
 				</Text>
